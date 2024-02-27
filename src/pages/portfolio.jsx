b7196a3dc6f2b174da5e7b52e73ea08f84d9f7da
Jianxiong Chu <xsjwj234884@gmail.com>
2024-02-26 20:49:01 -0500

    Portfolio Page Scaling Change

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,7 +1,7 @@
 import PortfolioCard from "../components/PortfolioCard";
 
 export default function Portfolio() {
-  return <div className="flex flex-col h-screen ml-48 mr-48">
+  return <div className="flex flex-col h-full ml-48 mr-48">
     <PortfolioCard cover="Cover1" title="Smartwatch Dashboard System"/>
   </div>;
 }
