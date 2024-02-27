6a904a92a022e540987dd62ad071a9aec04f5988
Jianxiong Chu <xsjwj234884@gmail.com>
2024-02-26 20:54:19 -0500

    Portfolio Page Deleted Card h-screen Class

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -3,7 +3,7 @@ import { Info, Covers } from './PortfolioCardInfo';
 
 export default function PortfolioCard({title}) {
   return (
-    <div className="flex flex-row h-screen m-4 rounded border-4 w-full h-[26em]">
+    <div className="flex flex-row m-4 rounded border-4 w-full h-[26em]">
       <img alt="cover" className="object-none w-1/3 rounded" src={Covers[title]} />
       <div className="m-4 w-2/3">
         <h2 className="text-3xl text-wrap m-4 font-bold">{title}</h2>
